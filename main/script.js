5dbde4709f2b9b0baa44944255a348b336d5bdbe
KINGSLEY <mamahkingsleychukwuebuka@gmail.com>
2023-08-24 14:53:35 +0100

    update

--- main/script.js
+++ main/script.js
@@ -1,6 +1,7 @@
+"use stict"
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
-const navSection = document.querySelector(".nav-section")
+
 
 hamburger.addEventListener("click", ()=>{
     hamburger.classList.toggle("active")
